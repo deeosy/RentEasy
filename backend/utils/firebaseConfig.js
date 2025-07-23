d2d194f11064ac560f2ca9dbab658e58d32b51ad
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-07-23 13:27:05 +0100

    1

--- backend/utils/firebaseConfig.js
+++ backend/utils/firebaseConfig.js
@@ -1,7 +1,7 @@
-const admin = require('firebase-admin');
 require('dotenv').config();
+const admin = require('firebase-admin');
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
